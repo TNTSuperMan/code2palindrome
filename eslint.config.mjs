07fac475168c501081d7ac6da58277653d600a79
TNTSuperMan <tntsuperga@gmail.com>
2025-02-27 17:46:22 +0900

    chore: setup eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
+const tabLength = 4;
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+    {files: ['**/*.{js,mjs,cjs,ts}']},
+    {languageOptions: { globals: globals.browser }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            'no-unused-vars': ['error'],
+            'no-undef': ['error'],
+            eqeqeq: ['error', 'always'],
+            'no-console': ['warn'],
+            indent: ['error', tabLength],
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            'brace-style': ['error', '1tbs'],
+            camelcase: ['error', { properties: 'always' }],
+            'no-magic-numbers': ['warn', { ignore: [0, 1] }],
+            'consistent-return': ['error'],
+            'no-var': ['error'],
+            complexity: ['warn', { max: 10 }],
+            'prefer-const': ['error'],
+        },
+    }
 ];
