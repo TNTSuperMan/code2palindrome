00bed7c85c05c6fe70a8a2b6d3b7274ac2aa71bf
TNTSuperMan <tntsuperga@gmail.com>
2025-02-27 17:56:37 +0900

    style: lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { readFileSync, writeFileSync } from "fs"
-import code2palindrome from "./module";
+import { readFileSync, writeFileSync } from 'fs';
+import code2palindrome from './module';
 
-const sourcePath = process.argv[2] //回文にするファイル
+const sourcePath = process.argv[2]; //回文にするファイル
 const outputPath = process.argv[3] ?? sourcePath; //出力先
 if(!sourcePath){
-    console.log("using: code2palindrome [target] [output?]")
+    console.log('using: code2palindrome [target] [output?]');
 }else{
-    const content = readFileSync(sourcePath).toString()
+    const content = readFileSync(sourcePath).toString();
     writeFileSync(outputPath, code2palindrome(content));
 }
